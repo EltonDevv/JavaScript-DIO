490e4bebf258dec5423c7f17d64c17814597e007
elton <eltonsouzaoliveira5@gmail.com>
2026-07-17 23:48:29 -0300

    feat: aula 3 vetores e objetos

---
+++ aula3VetoresEObjetos/script.js
@@ -0,0 +1,13 @@
+// o que são vetores ou arrays
+
+// como de clarar um array
+let array = ['string', 1, true];
+console.log(array);
+
+// pode quardar vatios tipos de dados
+let array2 = ['string', 1, true, [array3], [array4], [array5], [array6],]
+console.log(array2);
+
+// como acessar um indice de array
+console.log(array2[0]);
+
