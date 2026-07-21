9553aaf17b56479acd17138e345124eaf9e1e9a4
elton <eltonsouzaoliveira5@gmail.com>
2026-07-20 23:06:41 -0300

    feat: aula 5 final

---
+++ aula5FuncoesESuasParticularidades/script.js
@@ -0,0 +1,12 @@
+function funcao() {
+    console.log('Tudo certo Jovem!')
+}
+
+funcao();
+
+// Funções com parâmetros
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo);
+}
+
+mensagem('Tudo certo', 'jovem!');
