aa8a27748fbed4e8586dc01122b51508925c9335
elton <eltonsouzaoliveira5@gmail.com>
2026-07-21 22:52:31 -0300

    feat: aula 6 parte 1

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 // Funções Declarativas
-// 
-//
+function funcao () {
+    console.log("Mensagem vinda de uma função declarativa!");
+};
+
+funcao();
+
+// Expressões de funções
+var funcao = function funcaoDeExpressao () {
+    console.log("Mensagem vinda de uma expressão de função!");
+};
+
+funcao();
+
+var funcao = function () {
+    console.log("Mensagem vinda também de uma expressão de função!");
+};
+
+funcao();
+
+// Arrow Function
+var funcao = () => {
+    console.log("Essa daqui já é uma mensagem vinda de uma Arrow function!");
+};
+
+funcao();
